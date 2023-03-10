dcd0b0bb2a7ffaaacbd3e2e7a0af4b4961145e38
Emma Stephan <estephan@berkeley.edu>
2023-03-10 13:13:24 -0800

    initial spring 2023 files

---
+++ Spring2022/exoglove.ino
@@ -0,0 +1,45 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  /*
+
+
+   Notes:
+   - default deflated 
+   - automatically inflates
+   - both valves open by default
+   
+   Pseudocode:
+
+   main:
+   voltage_to_send = sensor_activate()
+    
+    
+   else 
+    deflate()
+    
+
+   sensor_activate() {
+     check if voltage is within a range of the previous value
+     store current voltage for next cycle
+     return specific voltage value to send to valve 
+     (determines amount to inflate finger)
+   }
+   
+
+   inflate() {
+     close exit valve
+   }
+
+   deflate() {
+     open exit valave
+   }
+     
+   */
+
+}
